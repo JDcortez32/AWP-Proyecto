d44f85a21da13ab05cbe0404fd6cf36daed5eda2
JDcortez32 <godk986@gmail.com>
2025-10-25 13:46:13 -0600

    Update app.js

--- SW/app.js
+++ SW/app.js
@@ -1,24 +1,11 @@
-//Verificar si el navegador web soporta service workers
 if ("serviceWorker" in navigator) {
-
-    //Lamar el metodo register para registrar un SW (Service Worker)
-    //El parametro /sw.js es la ruta del archivo del SW
     navigator.serviceWorker
         .register("./sw.js")
-
-        //then se ejecuta si el registro fue exitoso
-        //reg es un objeto tipo serviceworkerregistration con informacion del sw
-        .then((reg)=> console.log("Service Worker registrado:", reg))
-
-        //catch se ejecuta si ocurre un error un error en el registro
-        //err contiene el mensaje o detalle del error
-        .catch((error) => console.log("Error al registrar el SW:", err));
+        .then((reg) => console.log("Service Worker registrado:", reg))
+        .catch((error) => console.log("Error al registrar el SW:", error)); // error corregido
 }
 
-//Agregamos un evento clic al boton check
 document.getElementById("check").addEventListener("click", () => {
-
-    //Verificar si el SW controla la pagina actual
     if (navigator.serviceWorker.controller) {
         alert("El service worker esta activo y controlando la pagina.")
     } else {
@@ -26,7 +13,6 @@ document.getElementById("check").addEventListener("click", () => {
     }
 });
 
-//Area de notificacion
 if (Notification.permission === "default") {
     Notification.requestPermission().then((perm) =>{
         if (perm === "granted"){
